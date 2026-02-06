51f79f76679c08eb0df6e1afd1e755de70d18145
gkssla0945-lgtm <gkssla0945@naver.com>
2026-02-06 16:53:02 +0900

    feat: implement play_random function (#1)

--- 100_prisoners.py
+++ 100_prisoners.py
@@ -1,1 +1,27 @@
-team2-1
+import random
+
+def play_random(n):
+    pardoned = 0
+    in_drawer = list(range(100))
+    sampler = list(range(100))    
+
+    for _round in range(n):
+        random.shuffle(in_drawer) 
+        all_found = True 
+
+        for prisoner in range(100):
+            found = False
+            for reveal in random.sample(sampler, 50):
+                if in_drawer[reveal] == prisoner:
+                    found = True
+                    break
+
+            if not found:
+                all_found = False
+                break
+
+        
+        if all_found:
+            pardoned += 1
+
+    return pardoned / n * 100
